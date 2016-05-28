770c7521c02835c49f2fbc65380b6711a8c62c0d
gunsmoke.shin@gmail.com <gunsmoke.shin@gmail.com>
2016-05-28 23:36:50 +0900

    이미지 수정, PlaceActivity를 InformationFragment와 InputFragment로 분리함

---
+++ app/src/main/java/com/shinjaehun/fieldtrip/InputFragment.java
@@ -0,0 +1,23 @@
+package com.shinjaehun.fieldtrip;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by shinjaehun on 2016-05-28.
+ */
+public class InputFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_input, container, false);
+
+
+        return v;
+    }
+}
